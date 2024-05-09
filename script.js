53d760ba28ca19ce7f24c2d1df84d7529add60c1
alykang <kanga850@newschool.edu>
2024-05-08 23:49:15 -0400

    final

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-window.addEventListener("load".init);
+document.addEventListener('DOMContentLoaded', function () {
 
-const init = ()=>(
-        //has everything you're going to do
-)
+
+    const titlePage = document.querySelector('.title-page');
+    const loganStreet = document.querySelector('.body-texts'); // Ensure this is the correct element
+  
+    const observer = new IntersectionObserver(
+      function (entries) {
+        entries.forEach(function (entry) {
+          if (!entry.isIntersecting || entry.intersectionRatio < 0.5) {
+            loganStreet.classList.add('visible');
+            console.log(loganStreet);
+            console.log("Observer Triggered:", entries);
+
+          } else {
+            loganStreet.classList.remove('visible');
+          }
+        });
+      },
+      {
+        threshold: [0.5], // 50% visibility
+      }
+    );
+  
+    observer.observe(titlePage); // Ensure this is the correct element
+  });
+  
